c57bc3845cb1eac145e2a1af00b5d9f435c299cb
Daniel <danielclough@users.noreply.github.com>
2023-04-18 01:06:00 -0400

    border

--- frontend/src/lib/drag/grid.svelte
+++ frontend/src/lib/drag/grid.svelte
@@ -1,4 +1,4 @@
-<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+<svg style="border: 1px solid white" width="90%" height="50%" xmlns="http://www.w3.org/2000/svg">
   <defs>
     <pattern id="smallGrid" width="8" height="8" patternUnits="userSpaceOnUse">
       <path d="M 8 0 L 0 0 0 8" fill="none" stroke="gray" stroke-width="0.5"/>
@@ -9,5 +9,5 @@
     </pattern>
   </defs>
       
-  <rect width="100%" height="50%" fill="url(#grid)" />
+  <rect width="100%" height="100%" fill="url(#grid)" />
 </svg>
