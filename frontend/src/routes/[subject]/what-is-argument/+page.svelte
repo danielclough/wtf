c7b8cee26a2fddf0ef3273db5b6b645e87cd5490
Daniel <danielclough@users.noreply.github.com>
2023-04-19 00:47:32 -0400

    what is argument first draft

---
+++ frontend/src/routes/[subject]/what-is-argument/+page.svelte
@@ -0,0 +1,97 @@
+<h1>
+    <a href="https//www.etymonline.com/word/argument">
+        Argument (n.)
+    </a>
+</h1>
+
+<h2>
+    from Latin argumentum "a logical argument; evidence, ground, support, proof," from arguere "make clear, make known, prove".
+</h2>
+
+<hr />
+
+<h3>
+    Objectives
+</h3>
+<ul>
+    <li>
+        Better understand facts and the relationship between facts.
+    </li>
+    <li>
+        Identify credence gaps.
+        <br />
+        - What we don't know for certain.
+    </li>
+    <li>
+        Close credence gaps.
+    </li>
+    <li>
+        Make ethical decisions based on evidence and reason.
+    </li>
+</ul>
+
+<h3>
+    Everything is a Proposition (P,Q)
+</h3>
+<ul>
+    <li>
+        Claim = Proposition suggested to be True.
+    </li>
+    <li>
+        True Fact = Proposition understood to be True.
+    </li>
+    <li>
+        Incorrect Fact = Proposition understood to be False.
+    </li>
+</ul>
+
+<h3>
+    Propositional Relationships
+</h3>
+<ul>
+    <li>
+        Biconditional Equality:
+        <br />
+        - It is daytime IF AND ONLY IF the sun is above the horizon == TRUE
+        <br />
+        - It is daytime IF AND ONLY IF I can see the sun == FALSE
+        <br />
+        - Energy IS Mass multiplied by the speed of light squared == TRUE
+    </li>
+    <li>
+        Implication:
+        <br />
+        - IF I am near the Earth THAN gravity pulls me down == TRUE
+        <br />
+        - IF I am near the Earth THAN gravity pushes me up == FALSE
+        <br />
+        - BECAUSE (E = mc2) THEREFORE tiny particles can make big explosions == TRUE
+    </li>
+    <li>
+        Conjunction:
+        <br />
+        - I am human AND I live on the Earth == TRUE
+        <br />
+        - I am human AND I live in the Andromeda Galaxy == FALSE
+        <br />
+        - The United States HAPPENED TO make the first atomic bomb == TRUE
+    </li>
+    <li>
+        Negation:
+        <br />
+        - I am NOT a dog == TRUE
+        <br />
+        - I am NOT a human == FALSE
+        <br />
+        - Brazil DOES NOT have nuclear bombs == TRUE
+    </li>
+    <li>
+        Exclusive Disjunction:
+        <br />
+        - I am human OR I am a dog == TRUE
+        <br />
+        - I am human OR I am American == FALSE
+        <br />
+        - BECAUSE Brazil does not have nuclear bombs THEREFORE they cannot start a nuclear war == TRUE
+    </li>
+</ul>
